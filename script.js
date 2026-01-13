caa8ef9183b31e698cc1edfffee52d2dde62a2a6
Tshering Nidup <ceering.nidup.7@gmail.com>
2026-01-13 09:51:55 +0600

    Added a profession

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typed', {
-    strings: [' a Front End Developer', 'an ICT Trainer', ' a Coding Trainer', 'an Educator.'],
+    strings: [' a Front End Developer', 'an ICT Trainer', ' a Coding Trainer', 'an Educator', ' Prompt Engineer'],
     typeSpeed: 80,
     backSpeed: 80,
     loop: true
